a8e1d4845cad61bab78c68f196ec11010ddd2d4d
Janice Chung <142273559+janicechungg@users.noreply.github.com>
2024-07-18 14:06:24 -0500

    added single post functionality to all posts

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -41,22 +41,24 @@ export function Home() {
                 <div className="row">
                     {posts.map(post => (
                         <div className="col-lg-3 col-md-4 col-sm-6 mb-4" key={post._id}>
-                            <div className="card h-100 position-relative">
-                                {post.image && (
-                                    <img src={`http://localhost:5000/uploads/${post.image}`} className="card-img-top" alt={post.title} />
-                                )}
-                                <div className="card-body d-flex flex-column">
-                                    <h5 className="card-title">{post.title}</h5>
-                                    <p className="card-text">{post.summary}</p>
-                                    <p className="card-text"><small className="text-muted">By {post.author.username}</small></p>
-                                    <div className="card-text mt-auto">
-                                        <div dangerouslySetInnerHTML={{ __html: truncateContent(post.content) }} />
-                                        {post.content.length > 150 && (
-                                            <Link to={`/post/${post._id}`} className="btn btn-primary mt-2">Read More</Link>
-                                        )}
+                            <Link to={`/post/${post._id}`} className="text-decoration-none">
+                                <div className="card h-100 position-relative">
+                                    {post.image && (
+                                        <img src={`http://localhost:5000/uploads/${post.image}`} className="card-img-top" alt={post.title} />
+                                    )}
+                                    <div className="card-body d-flex flex-column">
+                                        <h5 className="card-title">{post.title}</h5>
+                                        <p className="card-text">{post.summary}</p>
+                                        <p className="card-text"><small className="text-muted">By {post.author.username}</small></p>
+                                        <div className="card-text mt-auto">
+                                            <div dangerouslySetInnerHTML={{ __html: truncateContent(post.content) }} />
+                                            {post.content.length > 150 && (
+                                                <span className="btn btn-primary mt-2">Read More</span>
+                                            )}
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
+                            </Link>
                         </div>
                     ))}
                 </div>
--- src/Pages/YourPosts.js
+++ src/Pages/YourPosts.js
@@ -68,27 +68,32 @@ export function YourPosts() {
                 <div className="row">
                     {posts.map(post => (
                         <div className="col-lg-3 col-md-4 col-sm-6 mb-4" key={post._id}>
-                            <div className="card h-100 position-relative">
-                                {post.image && (
-                                    <img src={`http://localhost:5000/uploads/${post.image}`} className="card-img-top" alt={post.title} />
-                                )}
-                                <div className="card-body d-flex flex-column">
-                                    <h5 className="card-title">{post.title}</h5>
-                                    <p className="card-text">{post.summary}</p>
-                                    <p className="card-text"><small className="text-muted">By {post.author.username}</small></p>
-                                    <div className="card-text mt-auto">
-                                        <div dangerouslySetInnerHTML={{ __html: truncateContent(post.content) }} />
-                                        {post.content.length > 150 ? (
-                                            <Link to={`/post/${post._id}`} className="btn btn-primary mt-2">Read More</Link>
-                                        ) : (
-                                            <i
-                                                className="fas fa-trash-alt trash-icon"
-                                                onClick={() => handleDelete(post._id)}
-                                            ></i>
-                                        )}
+                            <Link to={`/post/${post._id}`} className="text-decoration-none">
+                                <div className="card h-100 position-relative">
+                                    {post.image && (
+                                        <img src={`http://localhost:5000/uploads/${post.image}`} className="card-img-top" alt={post.title} />
+                                    )}
+                                    <div className="card-body d-flex flex-column">
+                                        <h5 className="card-title">{post.title}</h5>
+                                        <p className="card-text">{post.summary}</p>
+                                        <p className="card-text"><small className="text-muted">By {post.author.username}</small></p>
+                                        <div className="card-text mt-auto">
+                                            <div dangerouslySetInnerHTML={{ __html: truncateContent(post.content) }} />
+                                            {post.content.length > 150 ? (
+                                                <span className="btn btn-primary mt-2">Read More</span>
+                                            ) : (
+                                                <i
+                                                    className="fas fa-trash-alt trash-icon"
+                                                    onClick={(e) => {
+                                                        e.stopPropagation();
+                                                        handleDelete(post._id);
+                                                    }}
+                                                ></i>
+                                            )}
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
+                            </Link>
                         </div>
                     ))}
                 </div>
